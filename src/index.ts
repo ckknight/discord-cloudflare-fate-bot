ca4e960d978b2ca569eced102bae22245a422ed8
Cameron Knight <ckknight@gmail.com>
2021-09-12 00:15:27 +0000

    use @glenstack/cf-workers-discord-bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
 /// <reference lib="WebWorker" />
 /// <reference types="@cloudflare/workers-types" />
 
+import {
+  createSlashCommandHandler,
+  ApplicationCommand,
+  InteractionHandler,
+  Interaction,
+  InteractionResponse,
+  InteractionResponseType,
+} from '@glenstack/cf-workers-discord-bot';
+
+const helloCommand: ApplicationCommand = {
+  name: 'hello',
+  description: 'Bot will say hello to you!',
+};
+
+const helloHandler: InteractionHandler = async (
+  interaction: Interaction,
+): Promise<InteractionResponse> => {
+  const userID = interaction.member.user.id;
+
+  return {
+    type: InteractionResponseType.ChannelMessageWithSource,
+    data: {
+      content: `Hello, <@${userID}>!`,
+      allowed_mentions: {
+        users: [userID],
+      },
+    },
+  };
+};
+
+declare const APPLICATION_SECRET: string;
+
+const slashCommandHandler = createSlashCommandHandler({
+  applicationID: '356117733638799360',
+  applicationSecret: APPLICATION_SECRET,
+  publicKey: '510df19c289da6847bd4d00c946b94fae022a1f79e3e316cdc40f17163b2ee92',
+  commands: [[helloCommand, helloHandler]],
+});
+
 addEventListener('fetch', (event) => {
-  event.respondWith(
-    new Response('Hello worker!', {
-      headers: { 'content-type': 'text/plain' },
-    }),
-  );
+  event.respondWith(slashCommandHandler(event.request));
 });
